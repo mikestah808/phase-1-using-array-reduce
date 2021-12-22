42de0d5443957ff51cc6630c3d946ac48f330732
Michael Stafford <michaelstafford808@gmail.com>
2021-12-21 18:19:08 -0600

    commit tree 84566ac24ceca9ef9591a2211006eed2c5933cd4

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const reducer = (accumulator, item) => {
+//     let total = 0;
+//     const batteries = () => {
+//         let totalBatteries = 0;
+//         totalBatteries = item
+//         console.log(totalBatteries)
+//         return total += totalBatteries
+//     }
+//     return accumulator +=total
+// }
+
+
+// let totalBatteries = batteryBatches.reduce(reducer, 0);
+
+
+
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);
+
+console.log(totalBatteries);
